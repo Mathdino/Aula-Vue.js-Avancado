d150884d2b5a4ca8f1183635b6876c20c7ecd2e7
Matheus Vinicius Moraes Bernardino <matheusvmb@educ.guarulhos.gov>
2025-04-14 16:30:26 -0300

    Finalizado aula de Router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1>Essa é a home</h1>
+  <h1 @click="navegarCursos">Essa é a home</h1>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    navegarCursos() {
+      this.$router.push('cursos');
+      setTimeout(() => {
+        this.$router.push('acoes');
+      }, 2000);
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
